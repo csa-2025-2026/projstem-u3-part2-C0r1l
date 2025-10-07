3766adb2305152b847e4678e399b26ab804be5b3
C0r1l <carolygonzalez1458@gmail.com>
2025-10-07 21:46:11 +0000

    hola

--- Main.java
+++ Main.java
@@ -6,8 +6,25 @@ public class Main
   {
     Scanner scan = new Scanner(System.in);  // input from the keyboard
     /* ============= BEGIN SOLUTION ================================ */
+      System.out.println("Enter in a number");
+      double target = 12.345
+      double user_input = scan.nextDouble();
+
+    
+
+      if (user_input == target)
+      {
+
+        System.out.println("Yes!");
 
 
+      }
+
+
+      System.out.println("Enter an integer")
+
+      int value = 48
+      int user_int = scan.Next
 
 
     /* ============= END SOLUTION ================================== */
